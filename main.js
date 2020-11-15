2789a5bd00dfe54462f05f12926241e5f9857011
TiredBirb <72065138+anna636@users.noreply.github.com>
2020-11-15 11:40:16 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+(function () {
+
+  let rotateY = 0;
+  let rotateX = 0;
+
+  document.onkeydown = function (e) {
+    if (e.keyCode === 37) rotateY -= 10;
+    else if (e.keyCode === 38) rotateX += 10;
+    else if (e.keyCode === 39) rotateY += 10;
+    else if (e.keyCode === 40) rotateX -= 10;
+
+    document.querySelector('.cube').style.transform =
+      'rotateY(' + rotateY + 'deg)' +
+      'rotateX(' + rotateX + 'deg)';
+  }
+
+})(); 
